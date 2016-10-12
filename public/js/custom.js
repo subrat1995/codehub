dd173f93797542b68811e4777b000b45478c34c3
subrat1995 <subratk95@gmail.com>
2016-10-12 18:00:02 +0530

    readmore added and deployed succesfully to lost symbol

--- public/js/custom.js
+++ public/js/custom.js
@@ -261,5 +261,44 @@ jQuery(document).ready(function($) {
 		});
 		return false;
 	});
+/* */
+
+	// Read more
+
+/* */
+
+$(document).ready(function() {
+	var showChar = 450;
+	var ellipsestext = "...";
+	var moretext = "more";
+	var lesstext = "less";
+	$('.more').each(function() {
+		var content = $(this).html();
+
+		if(content.length > showChar) {
+
+			var c = content.substr(0, showChar);
+			var h = content.substr(showChar-1, content.length - showChar);
+
+			var html = c + '<span class="moreelipses">'+ellipsestext+'</span>&nbsp;<span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">'+moretext+'</a></span>';
+
+			$(this).html(html);
+		}
+
+	});
+
+	$(".morelink").click(function(){
+		if($(this).hasClass("less")) {
+			$(this).removeClass("less");
+			$(this).html(moretext);
+		} else {
+			$(this).addClass("less");
+			$(this).html(lesstext);
+		}
+		$(this).parent().prev().toggle();
+		$(this).prev().toggle();
+		return false;
+	});
+});
 
 });
